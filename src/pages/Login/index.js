b0ff83729181f130e72f1d96e85bb6f2f5c89e05
Yago Siguinolfi <yagosiguinolfi@gmail.com>
2021-04-15 13:04:03 -0300

    first styles project

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { Container, View, Image, BackgroundImage } from "../../utils/styles";
-import {colors} from '../../utils/colors';
+import { Container, View, Image, Text, Input, Button, Link } from "../../utils/styles";
+import { colors } from '../../utils/colors';
 
-import bkgLogin from '../../assets/images/background-login.jpg';
-import imgLogin from '../../assets/images/pernalonga.jpg';
+import bkgLogin from '../../assets/images/background-login-black.png';
+import imgComicsLogo from '../../assets/images/marvel-comics-logo.png';
+import { useRouteMatch } from "react-router";
 
 const Login = () => {
+  let match = useRouteMatch();
+
   return (
-    <Container>
-      <View width={'750px'} height={'500px'} radius={'20px'} color={colors.white}>
-        
+    <Container backgroundImage={bkgLogin}>
+      <View
+        width={'750px'}
+        height={'500px'}
+        radius={'20px'}
+        column
+        color={colors.white} >
+        <Image src={imgComicsLogo} width={'200px'} />
+        <View>
+        <View align={'flex-start'}>
+          <Text>Usuário</Text>
+          <Input />
+          <Text>Senha</Text>
+          <Input />
+          <Link size={'13px'} to={`/app`} >Esqueceu sua senha?</Link>
+          </View>
+          <View row>
+            <Button light borderWidth={2} borderColor={colors.red}><Text color={colors.red} bold>Cadastre-se</Text></Button>
+            <Button light borderWidth={2} borderColor={colors.red}><Text color={colors.red} bold onClick={() => this.authLogin()}>Login</Text></Button>
+          </View>
+        </View>
       </View>
     </Container>
   );
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,7 @@
 export const colors = {
   white: '#ffffff',
+  black: '#000000',
   blue: '#0000ff',
   red: '#ff0000',
+  yellow: '#ffff00',
 }
